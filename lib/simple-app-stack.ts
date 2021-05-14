9d6ca9b0cc61f589a0bef8c8f214168cc0ee7348
Gonzalo de la Torre <gonzalozujar@gmail.com>
2021-05-14 10:26:00 +0200

    Subida archivos

--- lib/simple-app-stack.ts
+++ lib/simple-app-stack.ts
@@ -2,17 +2,22 @@ import * as sns from '@aws-cdk/aws-sns';
 import * as subs from '@aws-cdk/aws-sns-subscriptions';
 import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda'
+import * as api from '@aws-cdk/aws-apigateway'
 
 export class SimpleAppStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'SimpleAppQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    // defines an AWS Lambda resource
+    const hello = new lambda.Function(this, 'PrimeraLambda', {
+      runtime: lambda.Runtime.NODEJS_10_X,    // execution environment
+      code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
+      handler: 'funcion.handler'                // file is "funcion", function is "handler"
     });
 
-    const topic = new sns.Topic(this, 'SimpleAppTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    new api.LambdaRestApi(this, 'PrimeraApi', {
+      handler: hello
+    });
   }
 }
